79902db2b5acd13c3564de62e706078c0c27a544
yj_kee <keeyanjie2013@gmail.com>
2020-04-07 22:55:39 +0800

    Basic setup of a website

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en-UK">
+
+<head>
+    <title>a chat box</title>
+    <meta charset="UTF-8">
+    <link rel='icon' href='chatboxfavicon.ico' type='image/x-icon'/ >
+    <link rel='stylesheet' href='styles.css'>
+</head>
+
+<body>
+    <?php
+    echo "<h1>How is your ".date("l")."?</h1>";
+    ?>
+</body>
+
+</html>
